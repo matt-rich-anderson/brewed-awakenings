a5f31b16c8d9478bb539a5cffbf3ec76218f1fa1
matt-rich-anderson <matt.rich.anderson@gmail.com>
2021-11-25 12:17:16 -0600

    added even listener

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,5 +1,20 @@
 import { getProducts } from "./database.js"
 
+document.addEventListener("click", (clickEvent) => {
+    const itemClicked = clickEvent.target
+    if (itemClicked.id.startsWith("product")) {
+        const [,productId] = itemClicked.id.split("--")
+        console.log(productId)
+    
+        for (const product of products) {
+            if (product.id === parseInt(productId)) {
+            return window.alert(`${product.name} costs $${product.price}.`)
+            }
+        }
+    }
+}
+)
+
 const products = getProducts()
 
 export const Products = () => {
