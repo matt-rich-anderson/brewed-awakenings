bf1bf84337e75419630365fa7420404f16acdf10
matt-rich-anderson <matt.rich.anderson@gmail.com>
2021-11-26 10:26:23 -0600

    additional stuff

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -15,26 +15,54 @@ export const Employees = () => {
     return html
 }
 
-document.addEventListener("click", (clickEvent) => {
-    let alertMessage = ""
-    const itemClicked = clickEvent.target
-    
-    if (itemClicked.id.startsWith("employee")) {
-        const [,employeeId] = itemClicked.id.split("--")
+// My solution ↓↓↓
 
-        for (const employee of employees) {
-            if (employee.id === parseInt(employeeId)) {
-                const matchedEmployee = employee
-                let qtySold = null
-                for (const order of orders) {
-                    if(matchedEmployee.id === order.employeeId){
-                        qtySold += 1
-                    }                    
+// document.addEventListener("click", (clickEvent) => {
+//     let alertMessage = ""
+//     const itemClicked = clickEvent.target
+    
+//     if (itemClicked.id.startsWith("employee")) {
+//         const [,employeeId] = itemClicked.id.split("--")
+
+//         for (const employee of employees) {
+//             if (employee.id === parseInt(employeeId)) {
+//                 const matchedEmployee = employee
+//                 let qtySold = null
+//                 for (const order of orders) {
+//                     if(matchedEmployee.id === order.employeeId){
+//                         qtySold += 1
+//                     }                    
+//                 }
+//                 return alertMessage = window.alert(`${matchedEmployee.name} sold ${qtySold}`)  
+//             }
+//         }
+
+//     } 
+// }
+// )
+
+// Book solution ↓↓↓
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if ( employee.id === parseInt(employeeId)) {
+                    let employeeSales = null
+                    const employeeOrders = orders.filter((order) => {
+                            if (order.employeeId === employee.id) {
+                                employeeSales += 1
+                            }
+                        }
+                    )
+
+                    return window.alert(` ${employee.name} sold ${employeeSales} products `)
                 }
-                return alertMessage = window.alert(`${matchedEmployee.name} sold ${qtySold}`)  
             }
         }
-
-    } 
-}
+    }
 )
